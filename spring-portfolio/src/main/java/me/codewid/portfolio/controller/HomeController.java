a70d276b5a37b949f72e462421db33dadc12a52b
Wonjun <Wonjun@main-PC>
2015-09-21 21:49:08 +0900

    basic template

--- spring-portfolio/src/main/java/me/codewid/portfolio/controller/HomeController.java
+++ spring-portfolio/src/main/java/me/codewid/portfolio/controller/HomeController.java
@@ -1,4 +1,4 @@
-package me.codewid.portfolio;
+package me.codewid.portfolio.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -33,7 +33,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		return "index";
 	}
 	
 }
